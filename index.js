4fea2dcc53397b60458159a792b8273814d0c662
Shantanu Awasthi <sa11g19@soton.ac.uk>
2025-03-10 15:02:20 +0000

    updated index.js to

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-// This is a React exercise. 10/03/2025
-// The App component manages the state of the input field (name and input) and then updaets the state.
+// this is a React exercise. 10/03/2025
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+// the App component manages the state of the input field (name and input) and then updaets the state.
 class App extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = { name: 'React Learner', input: '' };
+        super(props);
+        this.state = { 
+          name: 'React Learner', 
+          input: '', 
+          history: []
+        };
     }
 
     // state management
@@ -13,7 +20,18 @@ class App extends React.Component {
     
     // update state
     updateName = () => {
-      this.setState({ name: this.state.input });
+      if (this.state.input.trim() !== '') {
+        this.setState((prevState) => ({
+          name: prevState.input,
+          history: [...prevState.history, prevState.input],
+          input: ''
+        }));
+      }
+    };
+
+    // clear history
+    clearHistory = () => {
+        this.setState({ history: [] });
     };
   
     render() {
@@ -22,13 +40,27 @@ class App extends React.Component {
           <Hello name={this.state.name} />
           <input type="text" value={this.state.input} onChange={this.handleInputChange} />
           <button onClick={this.updateName}>Update Name</button>
+          <NameHistory history={this.state.history} clearHistory={this.clearHistory} />
         </div>
       );
     }
   }
   
-  // Hello component displays name using the 'name' property
-  const Hello = ({ name }) => <h1>Hello, {name}!</h1>;
+    // 'Hello' component displays name using the 'name' property
+    const Hello = ({ name }) => <h1>Hello, {name}!</h1>;
+
+    // 'NameHistory' component displays the history of names
+    const NameHistory = ({ history, clearHistory }) => (
+        <div>
+        <h2>Name History</h2>
+        <ul>
+            {history.map((name, index) => (
+            <li key={index}>{name}</li>
+            ))}
+        </ul>
+        <button onClick={clearHistory}>Clear History</button>
+        </div>
+    );
   
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(<App />);
